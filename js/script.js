a4a8fb553a3b401eaf702e7cb92f50d316f9ca87
Laura Machado Antunes Santos <lauramachadosscontato@gmail.com>
2026-06-15 14:03:27 -0300

    function transações

--- js/script.js
+++ js/script.js
@@ -23,4 +23,51 @@ function updateUI() {
 
     let income = 0;
     let expense = 0;
+
+    transactions.forEach((transaction) => {
+        if (transaction.type === "income") {
+            income += transaction.amount;
+        } else {
+            expense += transaction.amount;
+        }
+    });
+    transactions.forEach((transaction, index) => {
+        const li = document.createElement("li");
+
+        li.classList.add(
+            "transaction",
+            transaction.type === "income"
+                ? "transaction-income"
+                : "transaction-expense"
+        );
+
+        const icon =
+            transaction.type === "income"
+                ? '<i class="fas fa-arrow-up"></i>'
+                : '<i class="fas fa-arrow-down"></i>';
+
+        li.innerHTML = `
+      <div>
+        <strong>${transaction.description}</strong><br>
+        <small>${transaction.category}</small>
+      </div>
+
+      <div>
+        <span class="amount">${icon} ${formatCurrency(transaction.amount)}</span>
+        <button class="delete-btn" onclick="removeTransaction(${index})" title="Deletar">
+          <i class="fas fa-trash-alt"></i> Excluir
+        </button>
+      </div>
+    `;
+
+        list.appendChild(li);
+    });
+
+    const balance = income - expense;
+
+    animateValue(incomeEl, income);
+    animateValue(expenseEl, expense);
+    animateValue(balanceEl, balance);
+
+    saveData();
 }
